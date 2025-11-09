b1289d2173fc67e52bedcab0546aef6f66a26750
Kevin Kmetz <kev.kmetz@gmail.com>
2025-11-09 15:54:49 -0700

    Port over sigmoid function, and look up Dox syntax
    No unit tests yet.

---
+++ src/breezy/NeuralMath.hx
@@ -0,0 +1,21 @@
+/*
+ * breezy-neural-networks/src/breezy/NeuralMath.hx
+ */
+
+/**
+ * A class for static methods representing various mathematical functions used
+ * in neural networks.
+ */
+class NeuralMath {
+  /**
+   * Normalizes any floating-point value to between 0.0 and 1.0, inclusive.
+   * Extremely negative values skew closer to 0.0, and extremely positive values
+   * skew closer to 1.0.
+   *
+   * @param value Any floating-point value.
+   * @returns A floating-point value between 0.0 and 1.0, inclusive.
+   */
+  public static function sigmoid(value:Float):Float {
+    return 1.0 / (1.0 + Math.exp(-value));
+  }
+}
