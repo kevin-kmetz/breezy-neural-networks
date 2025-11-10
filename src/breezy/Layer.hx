0ed9cce06a30d5a99558cbeb760a40d907ef6e00
Kevin Kmetz <kev.kmetz@gmail.com>
2025-11-10 07:52:28 -0700

    Start implementing layer feed forward methods
    Nothing tested yet.
    
    Next up is to implement the remaining methdos needed in the feed forward process.

--- src/breezy/Layer.hx
+++ src/breezy/Layer.hx
@@ -16,7 +16,11 @@ class Layer {
   private final weights:Matrix;
   private final biases:Vector;
 
-  private function new(weights:Matrix, biases:Vector) {
+  private final activationFunc:Float -> Float;
+
+  private function new(
+    weights:Matrix, biases:Vector, activationFunc:Float -> Float
+  ) {
     this.neuronCount = weights.rows;
     this.inputNeuronCount = weights.columns;
 
@@ -24,12 +28,42 @@ class Layer {
     this.biases = biases;
   }
 
+  /**
+   * Creates a fully-connected layer for a multi-layer perceptron that has
+   * values for its weight matrix and bias vector that are randomly generated
+   * from a uniform distribution spanning -1.0 to 1.0 (inclusive and
+   * exclusive, respectively).
+   *
+   * @param neuronCount The number of neurons in the actual layer itself.
+   * @param inputNeuronCount The number of neurons in the preceding layer.
+   * @param activationFunc An activation function that takes a floating-point
+   *                       number and returns a floating-point number.
+   */
   public static function uniformlyRandomized(
-    neuronCount:Int, inputNeuronCount:Int
+    neuronCount:Int, inputNeuronCount:Int,
+    activationFunc:Float -> Float = breezy.NeuralMath.sigmoid
   ):Layer {
     final weights = Matrix.randomized(neuronCount, inputNeuronCount, -1.0, 1.0);
     final biases = Vector.randomized(neuronCount, -1.0, 1.0);
 
     return new Layer(weights, biases);
+  }
+
+  public function feedForward(inputActivation:Vector):Vector {
+    return applyActivation(
+             sumEdgesAddBias(
+               calculateEdges(inputActivation)
+             )
+           );
+  }
+
+  private function calculateEdges(inputActivation:Vector):Matrix {
+    return weights.multiplyVector(inputActivation);
+  }
+
+  private function sumEdgesAddBias(preActivation:Matrix):Vector {
+  }
+
+  private function applyActivation(preActivation:Vector):Vector {
   }
 }
